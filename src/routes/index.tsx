46d5512a99d3ff6d3c79d6744f67e862c1ee6936
TannerScadden <tanner@scaddenfamily.com>
2023-02-20 16:21:40 -0500

    Added mappings to change params to be accessors

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,14 @@
-import { Match, Switch, type VoidComponent } from "solid-js";
+import { createSignal, Match, Switch, type VoidComponent } from "solid-js";
 import { myQueryFn } from "~/server/queries";
 
 const Home: VoidComponent = () => {
-  const myNum = myQueryFn.createQuery(1, 2);
+  const [num1, setNum1] = createSignal(1);
+  const myNum = myQueryFn.createQuery(num1, () => 2);
   return (
     <Switch>
       <Match when={myNum.data}>
         <div>Num {myNum.data}</div>
+        <button onClick={() => setNum1((num) => num + 1)}>Increment</button>
       </Match>
       <Match when={myNum.error}>
         <div>Error</div>
--- src/server/query.ts
+++ src/server/query.ts
@@ -1,12 +1,13 @@
 import { createQuery } from "@adeora/solid-query";
 
+type RemapParams<T> = () => T;
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const query$ = <Fn extends (...args: any[]) => any>(queryFn: Fn) => {
   return {
-    createQuery: (...args: Parameters<Fn>) => {
+    createQuery: <Params = Parameters<Fn>>(...args: RemapParams<Params>[]) => {
       return createQuery(() => ({
-        queryKey: ["rpc", ...args],
-        queryFn: () => queryFn(...args),
+        queryKey: ["rpc", ...args.map((a) => a())],
+        queryFn: () => queryFn(...args.map((a) => a())),
       }));
     },
   };
