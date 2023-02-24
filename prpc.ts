0b14a74d1d3a74dc6de0a668a5653452dbf5db61
TannerScadden <tanner@scaddenfamily.com>
2023-02-24 16:01:27 -0500

    Got zed parsing in

--- prpc.ts
+++ prpc.ts
@@ -1,14 +1,16 @@
 import type { Plugin } from "vite";
 
 export default function prpc(): Plugin {
+  const queryRgx =
+    /query\$\((\s*\((?:[^)(]*|\((?:[^)(]*|\([^)(]*\))*\))*\))\s*=>\s*{([\s\S]*?)}\)/g;
+  const mutationRgx =
+    /mutation\$\((\s*\((?:[^)(]*|\((?:[^)(]*|\([^)(]*\))*\))*\))\s*=>\s*{([\s\S]*?)}\)/g;
+  const zodCheckRgx =
+    /\((\s*\w+\s*\)\s*=>[\s\S]*?)\s*,\s*(z\.object\([\s\S]*?\}\))\s*/g;
   return {
     enforce: "pre",
     name: "prpc",
     transform(code: string, id: string) {
-      const queryRgx =
-        /query\$\((\s*\((?:[^)(]*|\((?:[^)(]*|\([^)(]*\))*\))*\))\s*=>\s*{([\s\S]*?)}\)/g;
-      const mutationRgx =
-        /mutation\$\((\s*\((?:[^)(]*|\((?:[^)(]*|\([^)(]*\))*\))*\))\s*=>\s*{([\s\S]*?)}\)/g;
       if (
         id.endsWith(".ts") &&
         (queryRgx.test(code) || mutationRgx.test(code))
@@ -19,7 +21,23 @@ export default function prpc(): Plugin {
           code = `import server$ from "solid-start/server";\n${code}`;
         }
         return `${code
-          .replace(queryRgx, "query$(server$($1 => {$2}))")
+          .replace(queryRgx, (match, ...group: string[]) => {
+            if (zodCheckRgx.test(match)) {
+              return match.replace(zodCheckRgx, (_, ...zodGroup: string[]) => {
+                // This regex splits the arguments into their own groups.
+                const [func, zodSchema] = zodGroup;
+                // Need to split the args from the func body so we can insert the zod validations
+                const [args, funcBody] = func.split(`=> {\n`);
+
+                // Create the schema at the top of the function. Logging is for initial testing, we will want to remove it before it is prod ready. Them parse the args, but my regex sucks and I couldn't get the left ( around it so add it back in haha. Then execute the rest of the function body!
+                return `query$(server$((${args} => {\nconst schema = ${zodSchema};\nconsole.log(${args};\nschema.parse((${args});\n${funcBody}))\n`;
+              });
+            }
+            // If there isn't a zod object in the 2nd argument, do the normal parsing where we wrap server$ around the function and args.
+            const [args, functionBody] = group;
+            return `query$(server$(${args} => {${functionBody}}))`;
+          })
+          // I think we should make the query function parser reusable to avoid duplications if the argument structures are the same! Leaving this for now because there is an example of it above.
           .replace(mutationRgx, "mutation$(server$($1 => {$2}))")}`;
       }
       return null;
--- src/routes/query.tsx
+++ src/routes/query.tsx
@@ -1,4 +1,5 @@
 import {
+  createEffect,
   createSignal,
   Match,
   Suspense,
@@ -12,17 +13,20 @@ const Query: VoidComponent = () => {
   const addRes = add(
     () => ({
       a: num1(),
-      b: 2,
+      b: 3,
     }),
     () => ({
       placeholderData: (prev) => prev,
     })
   );
 
+  createEffect(() => console.log(JSON.parse(JSON.stringify(addRes))));
+
   return (
     <div class="flex flex-col gap-2 items-center my-16">
       <Suspense>
         <Switch>
+          <Match when={addRes.isLoading}>Loading...</Match>
           <Match when={addRes.data}>
             <div class="font-bold">Num {addRes.data}</div>
           </Match>
