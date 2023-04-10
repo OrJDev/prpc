ce34b13a397deb9d89d6b90180df38cb62179cba
OrJDev <orjdeveloper@gmail.com>
2023-04-10 22:26:32 +0300

    some docs

--- examples/nextjs/src/pages/index.tsx
+++ examples/nextjs/src/pages/index.tsx
@@ -2,7 +2,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 // import { query$ } from "@prpc/react";
-import { mutation$, query$ } from "@prpc/react";
+import { query$ } from "@prpc/react";
 import { type NextPage } from "next";
 import { z } from "zod";
 
@@ -17,45 +17,16 @@ const myQuery = query$({
   }),
 });
 
-const myMutation = mutation$({
-  mutationFn: ({ payload }) => {
-    console.log(payload);
-    return payload.a + payload.b;
-  },
-  schema: z.object({
-    a: z.number(),
-    b: z.number(),
-  }),
-  key: "testMut",
-});
-
 const Home: NextPage = () => {
-  const res = myQuery({
+  const { data, isLoading } = myQuery({
     num: 3,
   });
-  const mut = myMutation();
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-      {res.data ? (
-        <div className="text-5xl text-white">
-          query: thaler returned: {res.data}
-        </div>
-      ) : null}
-      <button
-        className="rounded-lg bg-[#ff00ff] p-4 text-2xl text-white"
-        onClick={() => {
-          mut.mutate({
-            a: 1,
-            b: 2,
-          });
-        }}
-      >
-        mutate
-      </button>
-      {mut.data ? (
-        <div className="text-5xl text-white">
-          mutation: thaler returned: {mut.data}
-        </div>
+      {isLoading ? (
+        <p className="text-xl font-bold text-white">Loading...</p>
+      ) : data ? (
+        <div className="text-5xl text-white">query: {data}</div>
       ) : null}
     </main>
   );
